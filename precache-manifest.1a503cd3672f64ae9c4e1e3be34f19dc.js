74d7ae2958b1ecd75cd8c9a59068bf8b43351932
khutso kenneth baloyi <khutso.baloyi@protonmail.com>
2021-12-03 14:45:56 +0200

    Updates

---
+++ precache-manifest.1a503cd3672f64ae9c4e1e3be34f19dc.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77784bb6e4b10e3202d03d50bf491822",
+    "url": "/crystalclear/index.html"
+  },
+  {
+    "revision": "aae09986509467305915",
+    "url": "/crystalclear/static/css/2.14f3cd7d.chunk.css"
+  },
+  {
+    "revision": "905820011d24a4044a3c",
+    "url": "/crystalclear/static/css/main.ddbae0ac.chunk.css"
+  },
+  {
+    "revision": "aae09986509467305915",
+    "url": "/crystalclear/static/js/2.0b67dbe1.chunk.js"
+  },
+  {
+    "revision": "6894069d2933e4dd35dea9d25383fdf2",
+    "url": "/crystalclear/static/js/2.0b67dbe1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "905820011d24a4044a3c",
+    "url": "/crystalclear/static/js/main.abf49599.chunk.js"
+  },
+  {
+    "revision": "badf57144acd6a3a79ee",
+    "url": "/crystalclear/static/js/runtime-main.b228117d.js"
+  },
+  {
+    "revision": "4c003c449037d0dc3ec199d6e402d5b0",
+    "url": "/crystalclear/static/media/crystalclear.4c003c44.png"
+  },
+  {
+    "revision": "81350ab88e99e27ff6a9f21dbafcaec3",
+    "url": "/crystalclear/static/media/crystalclear_prices.81350ab8.pdf"
+  },
+  {
+    "revision": "262298ff3fbc29a26d66340e822a7ff4",
+    "url": "/crystalclear/static/media/esther.262298ff.jpg"
+  },
+  {
+    "revision": "5aec2f2bd3720d6ef86b6977afc68a83",
+    "url": "/crystalclear/static/media/mass.5aec2f2b.JPG"
+  },
+  {
+    "revision": "22a0bffe789c286a9d78eb52670996a7",
+    "url": "/crystalclear/static/media/nucleo-outline.22a0bffe.ttf"
+  },
+  {
+    "revision": "24e2d6b43b1b0f84fdfaa06a4032f154",
+    "url": "/crystalclear/static/media/nucleo-outline.24e2d6b4.woff"
+  },
+  {
+    "revision": "53a1bed7a3ec86d010fe100873828a89",
+    "url": "/crystalclear/static/media/nucleo-outline.53a1bed7.eot"
+  },
+  {
+    "revision": "8ebec31f5ce59f908db84d86aed5947f",
+    "url": "/crystalclear/static/media/nucleo-outline.8ebec31f.woff2"
+  },
+  {
+    "revision": "a22c9182935649f87c660e02ce4d1531",
+    "url": "/crystalclear/static/media/relax.a22c9182.JPG"
+  },
+  {
+    "revision": "b29c2b5e3ed289efd64a91f75c1dfa74",
+    "url": "/crystalclear/static/media/school.b29c2b5e.JPG"
+  },
+  {
+    "revision": "9c5e01bda8e0d081fa9ff10ecb763a13",
+    "url": "/crystalclear/static/media/service1.9c5e01bd.jpg"
+  },
+  {
+    "revision": "5e828256108fd572bac54da996bcde64",
+    "url": "/crystalclear/static/media/service3.5e828256.jpg"
+  },
+  {
+    "revision": "1e909a9042f0f375f7883f313cbca70c",
+    "url": "/crystalclear/static/media/service4.1e909a90.jpg"
+  },
+  {
+    "revision": "ea943b5b39d533db9c532b7c16a4d8ef",
+    "url": "/crystalclear/static/media/service5.ea943b5b.jpg"
+  },
+  {
+    "revision": "4a34bbe8763754598d28884342221bb8",
+    "url": "/crystalclear/static/media/service6.4a34bbe8.jpg"
+  },
+  {
+    "revision": "3573286cbbb1b4d132d42c6ce89608c6",
+    "url": "/crystalclear/static/media/service7.3573286c.jpg"
+  },
+  {
+    "revision": "1a132125a7d73293608de7d9ac9b251d",
+    "url": "/crystalclear/static/media/service8.1a132125.jpg"
+  },
+  {
+    "revision": "a34871c5fe0cd84e1cb2936a4bd62e37",
+    "url": "/crystalclear/static/media/thato.a34871c5.jpg"
+  },
+  {
+    "revision": "34f15438aacb06966c85b1af399ea79c",
+    "url": "/crystalclear/static/media/tshidiso.34f15438.jpg"
+  }
+]);
